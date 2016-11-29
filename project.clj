7a707c5db2b37aeb3fac2e42cd67622ee04f3f9d
Tienson Qin <tienson@debian.debian>
2016-11-29 19:47:36 +0800

    add react-native-externs

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exponent/lein-template "0.2.7"
+(defproject exponent/lein-template "0.2.8"
   :description "Exponentjs template for Clojurescript."
   :url "https://github.com/tiensonqin/exponent-template"
   :license {:name "Eclipse Public License"
