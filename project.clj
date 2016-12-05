375dd6164cc69504d0d4bec3c91c4ecbe727148e
Tienson Qin <tienson@debian.debian>
2016-12-05 10:04:19 +0800

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exponent/lein-template "0.3.1"
+(defproject exponent/lein-template "0.3.3"
   :description "Exponentjs template for Clojurescript."
   :url "https://github.com/tiensonqin/exponent-template"
   :license {:name "Eclipse Public License"
