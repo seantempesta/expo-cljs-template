ab286378551e6e77a28b2e39cc658c03dbd9cd94
Sean Tempesta <sean.tempesta@gmail.com>
2017-08-31 19:57:05 +0200

    merging both ideas for ip address detection.  first look in .expo/settings.json for which connection type should be used (localhost, lan, or tunnel) and then try and guess the appropriate one.  And for an overide, just set .lan-ip

--- resources/leiningen/new/expo/env/dev/user.clj
+++ resources/leiningen/new/expo/env/dev/user.clj
@@ -1,6 +1,7 @@
 (ns user
   (:require [figwheel-sidecar.repl-api :as ra]
             [clojure.java.io :as io]
+            [clojure.data.json :as json]
             [clojure.string :as str]
             [hawk.core :as hawk]
             [clojure.tools.reader.edn :as edn]
@@ -29,197 +30,212 @@
   (-> "'use strict';\n\n// cljsbuild adds a preamble mentioning goog so hack around it\nwindow.goog = {\n  provide() {},\n  require() {},\n};\nrequire('./target/env/index.js');\n"
       ((partial spit "main.js"))))
 
+(defn get-expo-settings []
+  (try
+    (let [settings (-> (slurp ".expo/settings.json") json/read-str)]
+      settings)
+    (catch Exception e
+      nil)))
+
 (defn get-lan-ip
-  "If .lan-ip file exists, it fetches the ip from the file.
-   Othewise it attempts to get the ip from the system."
+  "If .lan-ip file exists, it fetches the ip from the file."
   []
   (if-let [ip (try (slurp ".lan-ip") (catch Exception e nil))]
-   (clojure.string/trim-newline ip)
-   (cond
-     (some #{(System/getProperty "os.name")} ["Mac OS X" "Windows 10"])
-     (.getHostAddress (java.net.InetAddress/getLocalHost))
+    (clojure.string/trim-newline ip)
+    (cond
+      (some #{(System/getProperty "os.name")} ["Mac OS X" "Windows 10"])
+      (.getHostAddress (java.net.InetAddress/getLocalHost))
 
-     :else
-     (->> (java.net.NetworkInterface/getNetworkInterfaces)
-          (enumeration-seq)
-          (filter #(not (or (str/starts-with? (.getName %) "docker")
-                            (str/starts-with? (.getName %) "br-"))))
-          (map #(.getInterfaceAddresses %))
-          (map
-           (fn [ip]
-             (seq (filter #(instance?
-                            java.net.Inet4Address
-                            (.getAddress %))
-                          ip))))
-          (remove nil?)
-          (first)
-          (filter #(instance?
-                    java.net.Inet4Address
-                    (.getAddress %)))
-          (first)
-          (.getAddress)
-          (.getHostAddress)))))
+      :else
+      (->> (java.net.NetworkInterface/getNetworkInterfaces)
+           (enumeration-seq)
+           (filter #(not (or (str/starts-with? (.getName %) "docker")
+                             (str/starts-with? (.getName %) "br-"))))
+           (map #(.getInterfaceAddresses %))
+           (map
+             (fn [ip]
+               (seq (filter #(instance?
+                               java.net.Inet4Address
+                               (.getAddress %))
+                            ip))))
+           (remove nil?)
+           (first)
+           (filter #(instance?
+                      java.net.Inet4Address
+                      (.getAddress %)))
+           (first)
+           (.getAddress)
+           (.getHostAddress)))))
+         
+  (defn get-expo-ip []
+    (if-let [expo-settings (get-expo-settings)]
+      (case (get expo-settings "hostType")
+        "lan" (get-lan-ip)
+        "localhost" "localhost"
+        "tunnel" (throw (Exception. "Expo Setting tunnel doesn't work with figwheel.  Please set to LAN or Localhost.")))
+      "localhost"))                                         ;; default
 
-(defn write-env-dev
-  []
-  (let [hostname (.getHostName (java.net.InetAddress/getLocalHost))
-        ip(get-expo-host)]
-    (-> "(ns env.dev)\n(def hostname \"%s\")\n(def ip \"%s\")"
-        (format
-         hostname
-         ip)
-        ((partial spit "env/dev/env/dev.cljs")))))
-
-(defn rebuild-env-index
-  [js-modules]
-  (let [modules (->> (file-seq (io/file "assets"))
-                     (filter #(and (not (re-find #"DS_Store" (str %)))
-                                   (.isFile %)))
-                     (map (fn [file] (when-let [unix-path (->> file .toPath .iterator iterator-seq (str/join "/"))]
-                                      (str "../../" unix-path))))
-                     (concat js-modules ["react" "react-native" "expo" "create-react-class"])
-                     (distinct))
-        modules-map (zipmap
-                     (->> modules
-                          (map #(str "\""
-                                     (if (str/starts-with? % "../../assets")
-                                       (-> %
-                                           (str/replace "../../" "./")
-                                           (str/replace "\\" "/")
-                                           (str/replace "@2x" "")
-                                           (str/replace "@3x" ""))
-                                       %)
-                                     "\"")))
-                     (->> modules
-                          (map #(format "(js/require \"%s\")"
-                                        (-> %
-                                            (str/replace "\\" "/")
-                                            (str/replace "@2x" "")
-                                            (str/replace "@3x" ""))))))]
-    (try
-      (-> "(ns env.index\n  (:require [env.dev :as dev]))\n\n;; undo main.js goog preamble hack\n(set! js/window.goog js/undefined)\n\n(-> (js/require \"figwheel-bridge\")\n    (.withModules %s)\n    (.start \"main\"))\n"
+  (defn write-env-dev
+    "First check the .expo/settings.json file to see what host is specified.  Then set the appropriate IP."
+    []
+    (let [hostname (.getHostName (java.net.InetAddress/getLocalHost))
+          ip (get-expo-ip)]
+      (-> "(ns env.dev)\n(def hostname \"%s\")\n(def ip \"%s\")"
           (format
-           (str "#js " (with-out-str (println modules-map))))
-          ((partial spit "env/dev/env/index.cljs")))
+            hostname
+            ip)
+          ((partial spit "env/dev/env/dev.cljs")))))
 
-      (catch Exception e
-        (println "Error: " e)))))
+  (defn rebuild-env-index
+    [js-modules]
+    (let [modules (->> (file-seq (io/file "assets"))
+                       (filter #(and (not (re-find #"DS_Store" (str %)))
+                                     (.isFile %)))
+                       (map (fn [file] (when-let [unix-path (->> file .toPath .iterator iterator-seq (str/join "/"))]
+                                         (str "../../" unix-path))))
+                       (concat js-modules ["react" "react-native" "expo" "create-react-class"])
+                       (distinct))
+          modules-map (zipmap
+                        (->> modules
+                             (map #(str "\""
+                                        (if (str/starts-with? % "../../assets")
+                                          (-> %
+                                              (str/replace "../../" "./")
+                                              (str/replace "\\" "/")
+                                              (str/replace "@2x" "")
+                                              (str/replace "@3x" ""))
+                                          %)
+                                        "\"")))
+                        (->> modules
+                             (map #(format "(js/require \"%s\")"
+                                           (-> %
+                                               (str/replace "\\" "/")
+                                               (str/replace "@2x" "")
+                                               (str/replace "@3x" ""))))))]
+      (try
+        (-> "(ns env.index\n  (:require [env.dev :as dev]))\n\n;; undo main.js goog preamble hack\n(set! js/window.goog js/undefined)\n\n(-> (js/require \"figwheel-bridge\")\n    (.withModules %s)\n    (.start \"main\"))\n"
+            (format
+              (str "#js " (with-out-str (println modules-map))))
+            ((partial spit "env/dev/env/index.cljs")))
 
-;; Each file maybe corresponds to multiple modules.
-(defn watch-for-external-modules
-  []
-  (let [path ".js-modules.edn"]
-    (hawk/watch! [{:paths ["src"]
-                   :filter hawk/file?
-                   :handler (fn [ctx {:keys [kind file] :as event}]
-                              (let [m (edn/read-string (slurp path))
-                                    file-name   (-> (.getPath file)
+        (catch Exception e
+          (println "Error: " e)))))
+
+  ;; Each file maybe corresponds to multiple modules.
+  (defn watch-for-external-modules
+    []
+    (let [path ".js-modules.edn"]
+      (hawk/watch! [{:paths   ["src"]
+                     :filter  hawk/file?
+                     :handler (fn [ctx {:keys [kind file] :as event}]
+                                (let [m (edn/read-string (slurp path))
+                                      file-name (-> (.getPath file)
                                                     (str/replace (str (System/getProperty "user.dir") "/") ""))]
 
-                                ;; file is deleted
-                                (when (= :delete kind)
-                                  (let [new-m (dissoc m file-name)]
-                                    (spit path new-m)
-                                    (rebuild-env-index (flatten (vals new-m)))))
+                                  ;; file is deleted
+                                  (when (= :delete kind)
+                                    (let [new-m (dissoc m file-name)]
+                                      (spit path new-m)
+                                      (rebuild-env-index (flatten (vals new-m)))))
 
-                                (when (.exists file)
-                                  (let [content (slurp file)
-                                        js-modules (some->>
-                                                    content
-                                                    (re-seq #"\(js/require \"([^\"]+)\"\)")
-                                                    (map last)
-                                                    (vec))
-                                        commented-modules (some->>
-                                                           content
-                                                           (re-seq #"[;]+[\s]*\(js/require \"([^\"]+)\"\)")
-                                                           (map last)
-                                                           (set))
-                                        js-modules (if commented-modules
-                                                     (vec (remove commented-modules js-modules))
-                                                     js-modules)]
-                                    (let [old-js-modules (get m file-name)]
-                                      (when (not= old-js-modules js-modules)
-                                        (let [new-m (if (seq js-modules)
-                                                      (assoc m file-name js-modules)
-                                                      (dissoc m file-name))]
-                                          (spit path new-m)
+                                  (when (.exists file)
+                                    (let [content (slurp file)
+                                          js-modules (some->>
+                                                       content
+                                                       (re-seq #"\(js/require \"([^\"]+)\"\)")
+                                                       (map last)
+                                                       (vec))
+                                          commented-modules (some->>
+                                                              content
+                                                              (re-seq #"[;]+[\s]*\(js/require \"([^\"]+)\"\)")
+                                                              (map last)
+                                                              (set))
+                                          js-modules (if commented-modules
+                                                       (vec (remove commented-modules js-modules))
+                                                       js-modules)]
+                                      (let [old-js-modules (get m file-name)]
+                                        (when (not= old-js-modules js-modules)
+                                          (let [new-m (if (seq js-modules)
+                                                        (assoc m file-name js-modules)
+                                                        (dissoc m file-name))]
+                                            (spit path new-m)
 
-                                          (rebuild-env-index (flatten (vals new-m)))))))))
-                              ctx)}])))
+                                            (rebuild-env-index (flatten (vals new-m)))))))))
+                                ctx)}])))
 
-(defn rebuild-modules
-  []
-  (let [path ".js-modules.edn"
-        m (atom {})]
-    ;; delete path
-    (when (.exists (java.io.File. path))
-      (clojure.java.io/delete-file path))
+  (defn rebuild-modules
+    []
+    (let [path ".js-modules.edn"
+          m (atom {})]
+      ;; delete path
+      (when (.exists (java.io.File. path))
+        (clojure.java.io/delete-file path))
 
-    (doseq [file (file-seq (java.io.File. "src"))]
-      (when (.isFile file)
-        (let [file-name (-> (.getPath file)
-                            (str/replace (str (System/getProperty "user.dir") "/") ""))
-              content (slurp file)
-              js-modules (some->>
-                          content
-                          (re-seq #"\(js/require \"([^\"]+)\"\)")
-                          (map last)
-                          (vec))
-              commented-modules (some->>
-                                 content
-                                 (re-seq #"[;]+[\s]*\(js/require \"([^\"]+)\"\)")
-                                 (map last)
-                                 (set))
-              js-modules (if commented-modules
-                           (vec (remove commented-modules js-modules))
-                           js-modules)]
-          (if js-modules
-            (swap! m assoc file-name (vec js-modules))))))
-    (spit path @m)
-    (rebuild-env-index (flatten (vals @m)))))
+      (doseq [file (file-seq (java.io.File. "src"))]
+        (when (.isFile file)
+          (let [file-name (-> (.getPath file)
+                              (str/replace (str (System/getProperty "user.dir") "/") ""))
+                content (slurp file)
+                js-modules (some->>
+                             content
+                             (re-seq #"\(js/require \"([^\"]+)\"\)")
+                             (map last)
+                             (vec))
+                commented-modules (some->>
+                                    content
+                                    (re-seq #"[;]+[\s]*\(js/require \"([^\"]+)\"\)")
+                                    (map last)
+                                    (set))
+                js-modules (if commented-modules
+                             (vec (remove commented-modules js-modules))
+                             js-modules)]
+            (if js-modules
+              (swap! m assoc file-name (vec js-modules))))))
+      (spit path @m)
+      (rebuild-env-index (flatten (vals @m)))))
 
-(defn init-external-modules
-  []
-  (rebuild-modules))
+  (defn init-external-modules
+    []
+    (rebuild-modules))
 
-;; Lein
-(defn start-figwheel
-  "Start figwheel for one or more builds"
-  [& build-ids]
-  (init-external-modules)
-  (enable-source-maps)
-  (write-main-js)
-  (write-env-dev)
-  (watch-for-external-modules)
-  (ra/start-figwheel!
-   {:figwheel-options {}
-    :build-ids  (if (seq build-ids)
-                  build-ids
-                  ["main"])
-    :all-builds (get-cljs-builds)})
-  (ra/cljs-repl))
+  ;; Lein
+  (defn start-figwheel
+    "Start figwheel for one or more builds"
+    [& build-ids]
+    (init-external-modules)
+    (enable-source-maps)
+    (write-main-js)
+    (write-env-dev)
+    (watch-for-external-modules)
+    (ra/start-figwheel!
+      {:figwheel-options {}
+       :build-ids        (if (seq build-ids)
+                           build-ids
+                           ["main"])
+       :all-builds       (get-cljs-builds)})
+    (ra/cljs-repl))
 
-(defn stop-figwheel
-  "Stops figwheel"
-  []
-  (ra/stop-figwheel!))
+  (defn stop-figwheel
+    "Stops figwheel"
+    []
+    (ra/stop-figwheel!))
 
-(defn -main
-  [args]
-  (case args
-    "--figwheel"
-    (start-figwheel)
+  (defn -main
+    [args]
+    (case args
+      "--figwheel"
+      (start-figwheel)
 
-    "--rebuild-modules"
-    (rebuild-modules)
+      "--rebuild-modules"
+      (rebuild-modules)
 
-    (prn "You can run lein figwheel or lein rebuild-modules.")))
+      (prn "You can run lein figwheel or lein rebuild-modules.")))
 
-;; Boot
-(defn prepare
-  []
-  (init-external-modules)
-  (enable-source-maps)
-  (write-main-js)
-  (write-env-dev)
-  (watch-for-external-modules))
+  ;; Boot
+  (defn prepare
+    []
+    (init-external-modules)
+    (enable-source-maps)
+    (write-main-js)
+    (write-env-dev)
+    (watch-for-external-modules))
