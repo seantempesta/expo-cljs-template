4abe54ad06b97cca0f1c6708e7ba3834be43d6ea
Sean Tempesta <sean.tempesta@gmail.com>
2017-09-12 21:06:19 +0200

    version bump to fix soblano.  thanks @tiensonqun

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject expo/lein-template "0.20.3"
+(defproject expo/lein-template "0.20.4"
   :description "Expo template for Clojurescript."
   :url "https://github.com/seantempesta/expo-cljs-template"
   :license {:name "Eclipse Public License"
